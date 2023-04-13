d61c76175282bbc6813d9eccf00e7cf0c1210e61
Ilaiah Gudepu <ilaiah.g@xrgsolutions.com>
2023-04-13 15:45:29 +0530

    Intial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// Variable Declarations
+
+let name='ilesh';  
+const dob=1996;
+
+//Types
+
+const a=null;
+console.log(typeof a);
+
+let undefinedType;
+console.log(typeof undefinedType)
+
+console.log(typeof name);
+console.log(typeof dob);
+
+let arrayType=[];
+console.log(typeof arrayType);
+
+let objType={};
+console.log(typeof objType);
